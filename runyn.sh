19127bf8c6107303934c5755bf7f309ec0e521c1
Timothy Spann <tspann@hortonworks.com>
2018-09-24 15:04:21 -0400

    Updated

---
+++ runyn.sh
@@ -0,0 +1,2 @@
+cd /Volumes/TSPANN/2018/talks/ApacheDeepLearning101/nifi-gluoncv-yolo3
+python3.6  -W ignore /Volumes/TSPANN/2018/talks/ApacheDeepLearning101/nifi-gluoncv-yolo3/yolonifi.py 2>/dev/null
